c4c912b0253aa4a24e71232a104a356b40a12d11
CastleJo <52406729+jo631@users.noreply.github.com>
2021-03-15 00:06:59 +0900

    fetchall, go routine

---
+++ Go/src/ch1/fetchall.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "1":
+		fetchAll()
+
+	}
+}
+
+func fetchAll() {
+	start := time.Now()
+	ch := make(chan string) // 문자열의 채널을 만듬
+
+	for _, url := range os.Args[2:] {
+		go fetch(url, ch) // go 루틴 시작
+	}
+	for range os.Args[2:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	if !strings.HasPrefix(url, "https://") {
+		url = "https://" + url
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
